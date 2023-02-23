7d5070641fff2cd653520bbd1b485ef225da6d8b
Edouard Mathieu <edmat@pm.me>
2023-02-23 11:55:06 +0100

    refactor: use harmonize_countries()

--- src/monkeypox.py
+++ src/monkeypox.py
@@ -2,10 +2,12 @@ import datetime
 
 import pandas as pd
 
+from owid.datautils import geo
+
 SOURCE_MONKEYPOX = (
     "https://frontdoor-l4uikgap6gz3m.azurefd.net/MPX/V_MPX_VALIDATED_DAILY?&$format=csv"
 )
-SOURCE_COUNTRY_MAPPING = "country_mapping.csv"
+SOURCE_COUNTRY_MAPPING = "country_mapping.json"
 SOURCE_POPULATION = "https://github.com/owid/covid-19-data/raw/master/scripts/input/un/population_latest.csv"
 OUTPUT_FILE = "owid-monkeypox-data.csv"
 
@@ -16,13 +18,22 @@ def import_data(url: str) -> pd.DataFrame:
 
 
 def clean_columns(df: pd.DataFrame) -> pd.DataFrame:
-    return df[["ISO3", "DATE", "TOTAL_CONF_CASES", "TOTAL_CONF_DEATHS"]].rename(
+    return df[["COUNTRY", "DATE", "TOTAL_CONF_CASES", "TOTAL_CONF_DEATHS"]].rename(
         columns={
-            "ISO3": "iso_code",
+            "COUNTRY": "location",
             "DATE": "date",
             "TOTAL_CONF_CASES": "total_cases",
             "TOTAL_CONF_DEATHS": "total_deaths",
         }
+    )
+
+
+def harmonize_countries(df: pd.DataFrame) -> pd.DataFrame:
+    return geo.harmonize_countries(
+        df,
+        countries_file=SOURCE_COUNTRY_MAPPING,
+        country_col="location",
+        warn_on_missing_countries=False,
     )
 
 
@@ -33,9 +44,9 @@ def clean_date(df: pd.DataFrame) -> pd.DataFrame:
 
 def clean_values(df: pd.DataFrame) -> pd.DataFrame:
     df = df.sort_values("date", ascending=False)
-    df["total_cases"] = df[["iso_code", "total_cases"]].groupby("iso_code").cummin()
-    df["total_deaths"] = df[["iso_code", "total_deaths"]].groupby("iso_code").cummin()
-    return df.sort_values(["iso_code", "date"])
+    df["total_cases"] = df[["location", "total_cases"]].groupby("location").cummin()
+    df["total_deaths"] = df[["location", "total_deaths"]].groupby("location").cummin()
+    return df.sort_values(["location", "date"])
 
 
 def explode_dates(df: pd.DataFrame) -> pd.DataFrame:
@@ -43,17 +54,17 @@ def explode_dates(df: pd.DataFrame) -> pd.DataFrame:
         [
             pd.DataFrame(
                 {
-                    "iso_code": iso_code,
+                    "location": location,
                     "date": pd.date_range(
                         start=df.date.min(), end=df.date.max(), freq="D"
                     ).astype(str),
                 }
             )
-            for iso_code in df.iso_code.unique()
+            for location in df.location.unique()
         ]
     )
     df = pd.merge(
-        df, df_range, on=["iso_code", "date"], validate="one_to_one", how="right"
+        df, df_range, on=["location", "date"], validate="one_to_one", how="right"
     )
     df["report"] = df.total_cases.notnull() | df.total_deaths.notnull()
     return df
@@ -61,8 +72,8 @@ def explode_dates(df: pd.DataFrame) -> pd.DataFrame:
 
 def add_world(df: pd.DataFrame) -> pd.DataFrame:
     df[["total_cases", "total_deaths"]] = (
-        df[["iso_code", "total_cases", "total_deaths"]]
-        .groupby("iso_code")
+        df[["location", "total_cases", "total_deaths"]]
+        .groupby("location")
         .ffill()
         .fillna(0)
     )
@@ -70,7 +81,7 @@ def add_world(df: pd.DataFrame) -> pd.DataFrame:
         df[["date", "total_cases", "total_deaths"]]
         .groupby("date", as_index=False)
         .sum()
-        .assign(iso_code="OWID_WRL", report=True)
+        .assign(location="World", report=True)
     )
     world = world[world.date < str(datetime.date.today())]
     return pd.concat([df, world])
@@ -79,11 +90,11 @@ def add_world(df: pd.DataFrame) -> pd.DataFrame:
 def add_population_and_countries(df: pd.DataFrame) -> pd.DataFrame:
     pop = pd.read_csv(
         SOURCE_POPULATION, usecols=["entity", "population", "iso_code"]
-    ).rename(columns={"entity": "location", "iso_code": "iso_code"})
-    missing_iso = set(df.iso_code) - set(pop.iso_code)
-    if len(missing_iso) > 0:
-        raise Exception(f"Missing ISO in population file: {missing_iso}")
-    df = pd.merge(pop, df, how="right", validate="one_to_many", on="iso_code")
+    ).rename(columns={"entity": "location"})
+    missing_locs = set(df.location) - set(pop.location)
+    if len(missing_locs) > 0:
+        raise Exception(f"Missing location(s) in population file: {missing_locs}")
+    df = pd.merge(pop, df, how="right", validate="one_to_many", on="location")
     return df
 
 
@@ -137,6 +148,7 @@ def main():
     (
         import_data(SOURCE_MONKEYPOX)
         .pipe(clean_columns)
+        .pipe(harmonize_countries)
         .pipe(clean_date)
         .pipe(clean_values)
         .pipe(explode_dates)
