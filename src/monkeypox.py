3a8e05ac6a5e0295b75c4227e8e65dc2e2bd699f
spoonerf <spoonerfiona@gmail.com>
2023-03-27 09:24:08 +0100

    fix(mpox): fetching data from one WHO region at a time

--- src/monkeypox.py
+++ src/monkeypox.py
@@ -2,16 +2,19 @@ import datetime
 
 import pandas as pd
 
-SOURCE_MONKEYPOX = (
-    "https://frontdoor-l4uikgap6gz3m.azurefd.net/MPX/V_MPX_VALIDATED_DAILY?&$format=csv"
-)
 SOURCE_COUNTRY_MAPPING = "country_mapping.csv"
 SOURCE_POPULATION = "https://github.com/owid/covid-19-data/raw/master/scripts/input/un/population_latest.csv"
 OUTPUT_FILE = "owid-monkeypox-data.csv"
+WHO_REGIONS = ["EURO", "AMRO", "WPRO", "EMRO", "AFRO", "SEARO"]
 
 
-def import_data(url: str) -> pd.DataFrame:
-    df = pd.read_csv(url)
+def import_data() -> pd.DataFrame:
+    df = pd.DataFrame()
+    # Fetching the data for each WHO region separately
+    for region in WHO_REGIONS:
+        url = f"https://frontdoor-l4uikgap6gz3m.azurefd.net/MPX/V_MPX_VALIDATED_DAILY?&$format=csv&$filter=WHO_REGION%20eq%20%27{region}%27"
+        df_region = pd.read_csv(url)
+        df = pd.concat([df, df_region])
     return df
 
 
@@ -135,7 +138,7 @@ def filter_dates(df: pd.DataFrame) -> pd.DataFrame:
 
 def main():
     (
-        import_data(SOURCE_MONKEYPOX)
+        import_data()
         .pipe(clean_columns)
         .pipe(clean_date)
         .pipe(clean_values)
